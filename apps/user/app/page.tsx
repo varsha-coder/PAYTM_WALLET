29fb5c3d7a28ef894a43db4ab6004bfb37a4da89
RK <2173176@cognizant.com>
2024-07-29 16:37:54 +0530

    till adding prisma

---
+++ apps/user/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+//import {PrismaClient} from "@repo/db/client"
+//const client= new PrismaClient();
+export default function Home() {  
+  return (
+   <div className="text-2xl">
+     from user app
+   </div>
+  );
+}
